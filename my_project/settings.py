c18443e4f243af79fecb9771241941edaaca1ee7
hagyeong <hagyeong13@naver.com>
2025-07-19 14:50:50 +0900

    login, logout 기능 추가

--- my_project/settings.py
+++ my_project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'product',
-    'pages'
+    'pages', 
+    'accounts.apps.AccountsConfig', 
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/' 
+LOGOUT_REDIRECT_URL = '/'
